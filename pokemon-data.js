f1b727a32778e7c012d8f2730180fc0db7cbddbd
yumna El Hendi <elhe0016@algonquinlive.com>
2023-04-16 03:59:36 -0400

    finalize project

--- pokemon-data.js
+++ pokemon-data.js
@@ -53,3 +53,79 @@ fetch('https://pokeapi.co/api/v2/pokemon/?limit=50')
       galleryContainer.appendChild(img);
     });
   });
+  
+// get caught pokemon list from local storage or initialize an empty array
+let caughtPokemonList = JSON.parse(localStorage.getItem('caughtPokemonList')) || [];
+
+// function to add a pokemon to the caught list
+const addPokemonToCaughtList = (pokemon) => {
+  // check if the pokemon is already in the caught list
+  const index = caughtPokemonList.findIndex(p => p.id === pokemon.id);
+  if (index !== -1) {
+    // pokemon already exists in the list, no need to add it again
+    return;
+  }
+  // add the pokemon to the caught list
+  caughtPokemonList.push(pokemon);
+  // save the updated caught list to local storage
+  localStorage.setItem('caughtPokemonList', JSON.stringify(caughtPokemonList));
+};
+
+// function to remove a pokemon from the caught list
+const removePokemonFromCaughtList = (pokemon) => {
+  // find the index of the pokemon in the caught list
+  const index = caughtPokemonList.findIndex(p => p.id === pokemon.id);
+  if (index !== -1) {
+    // pokemon found in the caught list, remove it
+    caughtPokemonList.splice(index, 1);
+    // save the updated caught list to local storage
+    localStorage.setItem('caughtPokemonList', JSON.stringify(caughtPokemonList));
+  }
+};
+
+// function to update the UI to reflect the caught status of a pokemon
+const updateCaughtStatus = (pokemon, isCaught) => {
+  // find the pokemon in the gallery and update its UI
+  const galleryPokemon = pokemonArray.find(p => p.id === pokemon.id);
+  if (galleryPokemon) {
+    const galleryPokemonElement = galleryContainer.querySelector(`[data-id="${pokemon.id}"]`);
+    if (isCaught) {
+      // add the "caught" class to the pokemon's UI
+      galleryPokemonElement.classList.add('caught');
+      // add the pokemon to the caught list
+      addPokemonToCaughtList(pokemon);
+    } else {
+      // remove the "caught" class from the pokemon's UI
+      galleryPokemonElement.classList.remove('caught');
+      // remove the pokemon from the caught list
+      removePokemonFromCaughtList(pokemon);
+    }
+  }
+};
+
+
+// function to initialize the caught status of all pokemon in the gallery
+const initializeCaughtStatus = () => {
+  caughtPokemonList.forEach(pokemon => {
+    const galleryPokemonElement = galleryContainer.querySelector(`[data-id="${pokemon.id}"]`);
+    if (galleryPokemonElement) {
+      // add the "caught" class to the pokemon's UI
+      galleryPokemonElement.classList.add('caught');
+    }
+  });
+};
+initializeCaughtStatus();
+
+// event listener for clicking on a pokemon in the gallery
+galleryContainer.addEventListener('click', event => {
+  const clickedElement = event.target.closest('.pokemon');
+  if (clickedElement) {
+    const pokemonId = parseInt(clickedElement.dataset.id);
+    const pokemon = pokemonArray.find(p => p.id === pokemonId);
+    if (pokemon) {
+      const isCaught = clickedElement.classList.contains('caught');
+      updateCaughtStatus(pokemon, !isCaught);
+    }
+  }
+});
+
